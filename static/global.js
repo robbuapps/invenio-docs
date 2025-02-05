2b9550026631bddf9ac08a13991383e8a846e17f
Milton Camara <miltoncamaragomes@gmail.com>
2025-02-05 08:08:00 -0300

    Adicionado configurações gerais na doc de importacao via ftp.

--- static/global.js
+++ static/global.js
@@ -1,5 +1,9 @@
-document.getElementById("btn-consultar-whatsapp").addEventListener("click", function () {
-    alert('clicou');
-});
-
-alert('clicou');
+<script src="https://webchatapi.robbu.global/v3/chat.js"></script>
+  chatRobbu.init("BFC66391CDB3DA97", {
+    open: true,
+    feedback: false,
+    logo: "https://storage.robbu.global/v2/file/6FB337BBB7809EECC8BCBD991CAB4795",
+    call: "Iniciar",
+    new_message_sound: true,
+    container: "#chat-robbu-container" // Garante que o chat será carregado dentro do div
+  });
