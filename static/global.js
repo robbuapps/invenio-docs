1fbf1e012041be5adf66fc4d8eda2d7aa61d4da2
Milton Camara <miltoncamaragomes@gmail.com>
2025-03-31 11:36:20 -0300

    removido o chat robbu

--- static/global.js
+++ static/global.js
@@ -1,8 +1,8 @@
-  chatRobbu.init("BFC66391CDB3DA97", {
-    open: true,
-    feedback: false,
-    logo: "https://storage.robbu.global/v2/file/6FB337BBB7809EECC8BCBD991CAB4795",
-    call: "Iniciar",
-    new_message_sound: true,
-    container: "#chat-robbu-container" // Garante que o chat será carregado dentro do div
-  });
+  // chatRobbu.init("BFC66391CDB3DA97", {
+  //   open: true,
+  //   feedback: false,
+  //   logo: "https://storage.robbu.global/v2/file/6FB337BBB7809EECC8BCBD991CAB4795",
+  //   call: "Iniciar",
+  //   new_message_sound: true,
+  //   container: "#chat-robbu-container" // Garante que o chat será carregado dentro do div
+  // });
